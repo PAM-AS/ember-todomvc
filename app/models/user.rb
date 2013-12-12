87db85391ff04dac12f3631e03e5dd5e9447752c
Kim Røen <kim@pam.as>
2013-12-12 21:04:48 +0100

    Turn off some validation now that we can do either

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  # has_secure_password
   has_many :api_keys
   has_many :todos, dependent: :destroy
 
-  validates :email, presence: true, uniqueness: true
-  validates :username, presence: true, uniqueness: true
+  validates :email, uniqueness: true
   validates :name, presence: true
 
   def session_api_key
